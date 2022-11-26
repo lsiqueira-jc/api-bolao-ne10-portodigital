1f8639e857ba41831f31800bd3f75ca740d6006c
Luiz Siqueira <luiz-tecnologia@MacBook-Pro-de-Luiz.local>
2022-11-26 10:40:34 -0300

    [fix]

--- src/database/entity/rodada.entity.ts
+++ src/database/entity/rodada.entity.ts
@@ -1,17 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
-export class User {
+export class rodada {
     @PrimaryGeneratedColumn()
     id: number
-
-    @Column()
-    nome: string
-
-    @Column()
-    email: string
-
-    @Column()
-    senha: string
-
 }
