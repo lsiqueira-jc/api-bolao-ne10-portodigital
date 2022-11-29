86b757956cc6ce09a7042ac972292b6f505e72e7
Luiz Siqueira <luiz-tecnologia@MacBook-Pro-de-Luiz.local>
2022-11-29 20:36:09 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import * as express from "express"
 import { Request, Response } from "express"
 import { User } from "./database/entity/user.entity"
 
-import { AppDataSource } from "./database/index";
 import { myDataSource } from "../app-data-source"
-
 
 myDataSource.initialize()
     .then(() => {
@@ -20,26 +18,59 @@ const app = express();
 
 app.use(express.json());
 
+//listar os jogos
+app.get("/",(request,response)=> {
+  //consultar o banco de dados 
+  return response.json(["Brasil x Argentina", "Portugal X Espanha"]);
+});
+
+//Palpite Ricardo e Sandra 
+app.post("/palpitar",(request, response) =>{});
+
+//cadastro de usuario - Thiago e Eduardo 
+app.post('/cadastro', async (request,response)=>{
+const data = request.body;
+
+const usuario = new User();
+usuario.nome = data.nome
+usuario.email = data.email
+usuario.senha = data.senha  // https://github.com/kelektiv/node.bcrypt.js 
+
+await myDataSource.manager.save(usuario)
+
+return response.json(usuario);
+
+
+
+});
+
+//Logar  ????
+app.post('/logar',(request,response)=>{});
+
+//Ranking ????
+app.get('ranking',()=>{});
+
+
 
 // GET| POST | PUT | DELETE
 //Metodo GET - Ranking | Listagem de jogos
 
-app.post("/salvar", async (request, response) => {
-  const users = await myDataSource.getRepository(User).find()
+// app.post("/salvar", async (request, response) => {
+//   const users = await myDataSource.getRepository(User).find()
 
-  //1. Receber os dados
-  //2. Validar dados
-  //3. Validar jogos
-  //4. Salvar o jogo no banco
-  const { teste } = request.body;
-  console.log(teste);
-  return response.send("ap1");
-});
+//   //1. Receber os dados
+//   //2. Validar dados
+//   //3. Validar jogos
+//   //4. Salvar o jogo no banco
+//   const { teste } = request.body;
+//   console.log(teste);
+//   return response.send("ap1");
+// });
 
-app.get("/listar",async (request, response) => {
-  const users = await myDataSource.getRepository(User).find()
+// app.get("/listar",async (request, response) => {
+//   const users = await myDataSource.getRepository(User).find()
 
-  return response.json(["Brasil x Argentina", "Portugal X Espanha"]);
-});
+//   return response.json(["Brasil x Argentina", "Portugal X Espanha"]);
+// });
 
 app.listen(3333);
